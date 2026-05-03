9c326c41a9f751e19a576faa90349a7f5e26e610
Saurabh Sawant <saurabhsawant1599@gmail.com>
2026-05-03 04:31:46 +0200

    Remove base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://saurabh1599.github.io',
-  base: '/SaurabhSawant.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
